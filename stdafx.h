b4edef3a3e88c911c24895f71ce06cc329600058
wz111 <lxz1506buaa@163.com>
2017-10-01 10:41:23 +0800

    first edition

---
+++ stdafx.h
@@ -0,0 +1,16 @@
+
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <time.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+// TODO: 
